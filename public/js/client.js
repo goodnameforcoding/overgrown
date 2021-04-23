08bd407062cfcd43da4d073702941e966b1d6ed8
hallie <halsted.larsson@gmail.com>
2021-04-23 16:22:20 -0400

    Now correctly loading level data

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,36 @@
- //how to involve the player / user more
-
 console.log("text");
 const url = 'https://spreadsheets.google.com/feeds/cells/1G_W2VwxkauhjoE3akiS6VK_2NNZG02wBsrp2KiQ4_gM/1/public/full?alt=json';
 window.onload = function() {
 	console.log("window loaded");
 	let app;
 	let scenes = {
+		lv1sg1cap: {
+			url: "media/level1/LV1SG1CAP_BLACKBG.mp4",
+			sceneType: "skill",
+			nextScene: "",
+		},
+		lv1sg1nurse: {
+			url: "media/level1/LV1SG1NURSE_BLACKBG.mp4",
+			sceneType: "skill",
+			nextScene: "",
+		},
+		lvl1sg1buff: {
+			url: "media/level1/LV1SG1BUFF_BLACKBG.mp4",
+			sceneType: "skill",
+			nextScene: "",
+		},
 		yourFirstBattle: {
 
 			url: "media/yourFirstBattle.mp4",
 			bpm: 140,
 			sceneType: "battle",
 			startPhase: 7.2,
-			nextSceneFunc: function(gameState) {
-				return "yourFirstBattle";
-			},
+			decisions: [
+				{text: "Captain", targetScene: "lv1sg1cap" },
+				{text: "Nurse", targetScene: "lv1sg1nurse" },
+				{text: "Buff Wizard", targetScene: "lvl1sg1buff" },
+
+			],
 		}
 	}
 
@@ -62,6 +78,7 @@ $(document).keydown(function(e) {
 				strokeColor: "rgba(177,245,157, 0.5)",
 				speed: 300,
 				record: false,
+				decisions: null,
 				startPhase: 7.2,
 				offset: 100,
 				video: null,
@@ -76,11 +93,11 @@ $(document).keydown(function(e) {
 				tolerance: 0.2,
 			}
 		},
-		created: function() {
-			$(document).keydown(this.handleKeyPress);
-			this.loadJson(1).then(jsonData => {
-				console.log(jsonData);
-			});
+		 created: async function() {
+			let jsonData = await this.loadJson(1);
+			let baseSheet = this.parseSheet(jsonData);
+			this.startScene();
+
 		},
 		computed: {
 			videoUrl: function() {
@@ -88,9 +105,58 @@ $(document).keydown(function(e) {
 			}
 		},
 		methods: {
+			startScene: function() {
+				this.decisions = null;
+				this.bpm = this.scene.bpm;
+				this.startPhase = this.scene.startPhase;
+				this.tick();
 
+			},
+			parseSheet(sheetJson) {
+				let entries = sheetJson.feed.entry;
+				let colCount = sheetJson.feed.gs$colCount.$t;
+				let rowCount = sheetJson.feed.gs$rowCount.$t;
+				let rows = [];
+				let out = [];
+				let headers = [];
+				let rowMax = 0;
+				for(let row = 0; row < rowCount; row++) {
+					rows[row] = {cols: []};
+				}
+				for (entry of entries) {
+					let col = entry['gs$cell'].col - 1;
+					let row =  entry['gs$cell'].row - 1;
+					rowMax = Math.max(row, rowMax);
+					console.log(row, rowMax)
+					let cell = { col , row, content: entry.content['$t']};
+					console.log(cell);
+					if(row == 0) {
+						headers[col] = cell.content;
+					} else if(cell.content) {
+						rows[cell.row].cols[cell.col] = cell;
+					}
+				}
+				for (let i = 1; i <= rowMax; i++) {
+					let row = rows[i];
+					let object = {};
+					for(let keyIndex in headers) {
+						let key = headers[keyIndex];
+						object[key] = row.cols[keyIndex]? row.cols[keyIndex].content : null;
+					}
+					out.push(object);
+				}
+				console.log(out);
+				return rows;
+			},
 			async loadJson(sheetNumber) {
-				return await fetch(`https://spreadsheets.google.com/feeds/cells/${this.sheetCode}/${sheetNumber}/public/full?alt=json`).then(response => response.json());
+				console.log("Loading scene...");
+				let response =  await fetch(`https://spreadsheets.google.com/feeds/cells/${this.sheetCode}/${sheetNumber}/public/full?alt=json`);
+				return response.json();
+			},
+			async makeDecision(decision) {
+				this.scene = decision.targetScene;
+				this.startScene();
+
 			},
 			handleKeyPress(e) {
 
@@ -137,12 +203,20 @@ $(document).keydown(function(e) {
 					this.t = video? video.currentTime : 0;
 				
 					if (this.t >= duration) {
-						this.scene = this.scenes[this.scene.nextSceneFunc(this.data)];
-						video.currentTime = 0;
-						video.play();	
+						this.endScene(this.scene);
+					} else {
+						setTimeout(this.tick, 1000/ this.fps);
 					}
-					setTimeout(this.tick, 1000/ this.fps);
 				}
+			},
+			endScene: function(scene) {
+				if(scene.decisions) {
+					this.decisions = scene.decisions;
+				}
+			},
+			makeDecision: function(decisionMade) {
+				console.log(decisionMade);
+				this.scene = this.scenes[decisionMade.targetScene];
 			},
 			saveNotes: function() {
 				this.localStorage.notes = this.notes;
@@ -150,29 +224,29 @@ $(document).keydown(function(e) {
 			onVideoLoad: function(e) {
 				let video = e.target;
 				this.video = video;
-				let bpm = this.bpm;
-				let startPhase = this.startPhase;
-				let videoDuration = video.duration;
-				let songDuration = videoDuration - startPhase; 
-				let tickDistance = 60 / bpm;
-				console.log("onload", startPhase, videoDuration, tickDistance);
-				let storedNotes = window.localStorage.notes;
-				if(window.localStorage.notes) {
-					this.notes = storedNotes;
-				}
-				for(let i = startPhase; i < videoDuration ; i+=tickDistance) {					
-						this.ticks.push({t: i, size: 40, stroke: 3});
-						this.ticks.push({t: i + tickDistance / 2, size: 20, stroke: 2});
-						this.ticks.push({t: i + tickDistance / 3, size: 10, stroke: 1});
-						this.ticks.push({t: i + 2* tickDistance / 3, size: 10, stroke: 1});
-						//this.notes.push({t: i });
+				if(this.scene.bpm) {
+					this.bpm = this.scene.bpm;
+					let bpm = this.bpm;
+					let startPhase = this.startPhase;
+					let videoDuration = video.duration;
+					let songDuration = videoDuration - startPhase; 
+					let tickDistance = 60 / bpm;
+					console.log("onload", startPhase, videoDuration, tickDistance);
+					let storedNotes = window.localStorage.notes;
+					if(window.localStorage.notes) {
+						this.notes = storedNotes;
+					}
+					for(let i = startPhase; i < videoDuration ; i+=tickDistance) {					
+							this.ticks.push({t: i, size: 40, stroke: 3});
+							this.ticks.push({t: i + tickDistance / 2, size: 20, stroke: 2});
+							this.ticks.push({t: i + tickDistance / 3, size: 10, stroke: 1});
+							this.ticks.push({t: i + 2* tickDistance / 3, size: 10, stroke: 1});
+							//this.notes.push({t: i });
+					}					
 				}
 				this.tick();
 			},
 		},
-		created: function() {
-			this.tick();
-		}
 	});
 
 
